ee9f442b34dffc7c5119a50256fa7d17a0eeb64e
Anusha-118 <pallemanushaa@gmail.com>
2025-10-05 14:56:29 +0530

    new file

---
+++ Raja.java
@@ -0,0 +1,22 @@
+public class Raja
+{
+  public static void main(String args[])
+  {
+    int num=27;
+    byte num1=127;
+    short num2=3456;
+    long num3=100000000000000l;
+    float num4=458.9f;
+    double num5=456.88888888888888;
+    char num6='m';
+    String num8="anusha";
+    System.out.println(num);
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println(num4);
+    System.out.println(num5);
+    System.out.println(num6);
+  
+    System.out.println(num8);
+}
+}
