f6274efe836ea34abac21cddf04e88157669aa13
Anusha-118 <pallemanushaa@gmail.com>
2025-10-07 20:14:30 +0530

    new

---
+++ All.java
@@ -0,0 +1,42 @@
+class Intermediate
+{
+void Anusha()
+{
+System.out.println("Anusha");
+}
+}
+class Chemistry 
+{
+void Vali()
+{
+System.out.println("Vali");
+}
+}
+class Physics
+{ 
+void Mukesh()
+{
+System.out.println("Mukesh");
+}
+}
+class Maths 
+{
+void Pujitha()
+{
+System.out.println("Pujitha");
+}
+}
+class All
+{
+public static void main(String args[])
+{
+Intermediate i=new Intermediate();
+i.Anusha();
+Chemistry c=new Chemistry();
+c.Vali();
+Physics p=new Physics();
+p.Mukesh();
+Maths m=new Maths();
+m.Pujitha();
+}
+}
