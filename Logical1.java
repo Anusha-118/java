eeaacd6717c8d1f3dbff0c4e0e7d74dd1fc62a07
Anusha-118 <pallemanushaa@gmail.com>
2025-10-11 22:01:53 +0530

    new

---
+++ Logical1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Logical1
+{
+  public static void main(String args [])
+  {
+    Scanner sc=new Scanner(System.in);
+
+      System.out.println(" enter a value:");
+      int a=sc.nextInt();
+      System.out.println(" enter b value:");
+      int b=sc.nextInt();
+      if(a>2 || b>2)
+      {
+        System.out.println("both are greater");
+
+      }
+      else{
+        System.out.println("they are not greater");
+
+      }
+  }
+}
