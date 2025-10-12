0eb09b46c602d389489101ddfd935db1787e757a
Anusha-118 <pallemanushaa@gmail.com>
2025-10-12 11:22:22 +0530

    new

--- Logical1.java
+++ Logical1.java
@@ -7,16 +7,37 @@ class Logical1
 
       System.out.println(" enter a value:");
       int a=sc.nextInt();
-      System.out.println(" enter b value:");
-      int b=sc.nextInt();
-      if(a>2 || b>2)
+      // System.out.println(" enter b value:");
+      // int b=sc.nextInt();
+      // System.out.println(" enter c value:");
+      // int c=sc.nextInt();
+      //   if(a>2 || b>2)
+      // {
+      //   System.out.println("both are greater");
+
+      // }
+      // else{
+      //   System.out.println("they are not greater");
+       
+       
+      //  if((a>2&&b>2) || c>2)
+      //  {
+      //    System.out.println("both are greater");
+
+      //  }
+      //  else{
+      //    System.out.println("they are not greater");
+
+      if(!(a>5))
       {
-        System.out.println("both are greater");
+        System.out.println("a is greater");
 
       }
       else{
-        System.out.println("they are not greater");
+        System.out.println("b is greater");
+      }
+
+
 
       }
   }
-}
---
+++ Presidence.java
@@ -0,0 +1,9 @@
+class Presidence 
+{
+  public static void main(String args[])
+  {
+    int a=2 +5 * (6-4) / 5; 
+    System.out.println(a);
+}
+}
+
