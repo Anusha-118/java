37ce36fd2973d294137d05756a8fe73939f5126d
Anusha-118 <pallemanushaa@gmail.com>
2025-10-13 21:58:43 +0530

    new

---
+++ Conditions1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Conditions{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter marks:");
+    int marks= sc.nextInt();
+    if(marks>=35)
+    {
+        System.out.println("Pass");
+    }
+    else{
+          System.out.println("Fail");
+    }
+  }
+}
