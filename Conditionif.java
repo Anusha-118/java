61e5fec5e56585901b67d80fc3e479f28ab4d013
Anusha-118 <pallemanushaa@gmail.com>
2025-10-14 21:39:03 +0530

    new

--- Conditionif.java
+++ Conditionif.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Conditions1{
+class Conditionif{
   public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
     System.out.println("enter marks:");
