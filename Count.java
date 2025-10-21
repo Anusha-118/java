9de98678ab74a8103b8bf5a95f705b44b49bcfda
Anusha-118 <pallemanushaa@gmail.com>
2025-10-21 10:21:42 +0530

    new

--- Count.java
+++ Count.java
@@ -1,13 +1,26 @@
+
+import java.util.Scanner;
 class Count{
   public static void main(String args[])
   {
-    int digit=34567;
+  //   int digit=245;
+  //   int count=0;
+  //   while(digit>0)
+  //     digit=digit/10;
+  //     count++;
+  //     System.out.println(count);
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter a number:"); 
+     int number=sc.nextInt();
     int count=0;
-    while(digit>0){
-      digit=digit/10;
-      count++;
-      System.out.println(count);
+     while(number>0){
+    number=number/10;
+    count++;
+    System.out.println(count);
+     } 
+
+       
+       }
 
     }
-  }
-}
+  
