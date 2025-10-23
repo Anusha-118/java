c7b67020c3ec8b3dc65438cf185135a4d0d268d3
Anusha-118 <pallemanushaa@gmail.com>
2025-10-23 10:29:16 +0530

    new

---
+++ Method2.java
@@ -0,0 +1,17 @@
+public class Method2{
+public static void main(String args[]){
+  details("sai");
+}
+public static void details(String name){
+  System.out.println(name);
+}
+}
+// class Method2 {
+//   static void demo(int a) {
+//     System.out.println(a);
+//   }
+
+//   public static void main(String args[]) {
+//     demo(23); // Works directly because demo is static
+//   }
+// }
