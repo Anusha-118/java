be274a38e02c179acb62e80036cc748d0cb1a07e
Anusha-118 <pallemanushaa@gmail.com>
2025-11-02 11:15:14 +0530

    new

---
+++ Calculators.java
@@ -0,0 +1,7 @@
+class Calculators{
+  public static void main(String args[]){
+    Operator op=new Operator();
+    double result= op.multiply(1,20);
+    System.out.println(result);
+  }
+}
--- Operator.java
+++ Operator.java
@@ -1,10 +1,5 @@
 class Operator{
-public static void main(String[] args) {
-  Operator o=new Operator();
-   double a=o.add(1.0,2);
-     System.out.println(a);
-  
-}
+
 
   
   public double add(double a, int b){
