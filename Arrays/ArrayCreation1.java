4de4e88c37ca29aace869e040f009d96955875de
Anusha Pallem <anushapallem459@gmail.com>
2025-12-14 23:09:04 +0530

    CountDigit

--- Arrays/ArrayCreation1.java
+++
@@ -1,7 +0,0 @@
-class ArrayCreation1{
-    public static void main(String args[]){
-        int[] marks={80,70,88,99,96};
-        // for(int i=0;i<marks.length;i++){
-        System.out.println(marks.length);
-    }
-}
